701c69fe98e46e364d82c4157f5076094d044977
HarshaThombre <thombreharsha@gmail.com>
2022-07-11 16:40:28 -0700

    commited by commad

--- src/App.js
+++ src/App.js
@@ -1,51 +1,62 @@
-
-
-import './App.css';
-import About from './components/About';
-import Navbar from './components/Navbar';
-import TextForm from './components/TextForm';
-import React, { useState } from 'react';
-import Alert from './components/Alert';
-
+import "./App.css";
+import About from "./components/About";
+import Navbar from "./components/Navbar";
+import TextForm from "./components/TextForm";
+import React, { useState } from "react";
+import Alert from "./components/Alert";
+import withRouter from "./components/withRouter";
+import { Routes, Route, Link } from "react-router-dom";
 
 function App() {
-const [mode, setMode] = useState('light'); // whetherdark mode is enable or not
-const[alert, setAlert] = useState(null);
+  const [mode, setMode] = useState("light"); // whetherdark mode is enable or not
+  const [alert, setAlert] = useState(null);
 
-const showAlert = (message, type)=>{
-setAlert({
-  msg: message,
-  type: type
-})
-setTimeout(()=>{
-setAlert(null);
-},1500);
-}
-const toggleMode = ()=>{
-  if(mode === 'light'){
-  setMode('dark');
-  document.body.style.backgroundColor = '#042743';
-  showAlert("Dark mode has been enabled", "success");
-  }
-  else{
-    setMode('light');
-    document.body.style.backgroundColor = 'white';
-    showAlert("light mode has been enabled", "success");
+  const showAlert = (message, type) => {
+    setAlert({
+      msg: message,
+      type: type,
+    });
+    setTimeout(() => {
+      setAlert(null);
+    }, 1500);
+  };
+  const toggleMode = () => {
+    if (mode === "light") {
+      setMode("dark");
+      document.body.style.backgroundColor = "#042743";
+      showAlert("Dark mode has been enabled", "success");
+    } else {
+      setMode("light");
+      document.body.style.backgroundColor = "white";
+      showAlert("light mode has been enabled", "success");
     }
-} 
-  return(
-<>
-{/* <Navbar title = "TextUtils" aboutText = "About TextUtils"/> */}
- {/* <div className="container"></div> */}
- <Navbar title = "TextUtils" mode = {mode} toggleMode = {toggleMode}/>
- <Alert alert ={alert}/>
-<div className="container my-3">
-<TextForm heading = "Enter the Text to analyze" mode={mode} showAlert={showAlert}/>
-{/* <About/> */}
-</div>
+  };
+  return (
+    <>
+      {/* <Navbar title = "TextUtils" aboutText = "About TextUtils"/> */}
+      {/* <div className="container"></div> */}
+      <Navbar title="TextUtils" mode={mode} toggleMode={toggleMode} />
+      <Alert alert={alert} />
+      <div className="container my-3">
+        
+          <Routes>
+            <Route path="/about" element={<About />}></Route>
 
-</>
-);
+            <Route
+              path="/"
+              element={
+                <TextForm
+                  heading="Enter the Text to analyze"
+                  mode={mode}
+                  showAlert={showAlert}
+                />
+              }
+            ></Route>
+          </Routes>
+        
+      </div>
+    </>
+  );
 }
 
-export default App;
+export default withRouter(App);
